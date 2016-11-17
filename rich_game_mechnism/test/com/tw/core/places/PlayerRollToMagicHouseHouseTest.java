3b1529e9d1213dc6a424ecf43162c90d1e2fd498
Peizhen Zheng <pzzheng@thoughtworks.com>
2016-11-17 09:21:44 +0800

    roll to magic house

---
+++ rich_game_mechnism/test/com/tw/core/places/PlayerRollToMagicHouseHouseTest.java
@@ -0,0 +1,54 @@
+package com.tw.core.places;
+
+import com.tw.core.Dice;
+import com.tw.core.Game;
+import com.tw.core.GameMap;
+import com.tw.core.Player;
+import com.tw.core.commands.Command;
+import com.tw.core.commands.CommandFactory;
+import com.tw.core.commands.Roll;
+import com.tw.core.responses.Response;
+import com.tw.core.tools.Gift;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by pzzheng on 11/17/16.
+ */
+public class PlayerRollToMagicHouseHouseTest {
+
+    public static final int INITIAL_FUND = 10000;
+    private GameMap map;
+    private Dice dice;
+    private MagicHouse magicHouse;
+
+    @Before
+    public void setUp() {
+        map = mock(GameMap.class);
+        dice = mock(Dice.class);
+        magicHouse = new MagicHouse();
+    }
+
+    @Test
+    public void should_wait_for_response() {
+        when(map.move(anyObject(), anyInt())).thenReturn(magicHouse);
+        Game game = new Game(map);
+        Player player = Player.createPlayerWithGame_Fund_CommandState(game, INITIAL_FUND);
+
+        assertThat(player.getStatus(), is(Player.Status.WAIT_FOR_COMMAND));
+
+        Command roll = CommandFactory.Roll(dice);
+        player.execute(roll);
+
+        assertThat(player.getStatus(), is(Player.Status.WAIT_FOR_TURN));
+        assertThat(player.getCurrentPlace(), is(magicHouse));
+        assertThat(player.lastCommand() instanceof Roll, is(true));
+    }
+}
