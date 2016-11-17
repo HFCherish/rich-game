25155bbd60318e249bcdd5e7210ccd92f8d9bc0a
Peizhen Zheng <pzzheng@thoughtworks.com>
2016-11-17 09:37:49 +0800

    pass mineral

---
+++ rich_game_mechnism/src/com/tw/core/places/Mineral.java
@@ -0,0 +1,22 @@
+package com.tw.core.places;
+
+import com.tw.core.Player;
+
+/**
+ * Created by pzzheng on 11/17/16.
+ */
+public class Mineral implements Place{
+    private int points;
+
+    public Mineral(int points) {
+        this.points = points;
+    }
+
+
+    @Override
+    public Player.Status comeHere(Player player) {
+        player.moveTo(this);
+        player.getAsests().addPoints(points);
+        return player.endTurn();
+    }
+}
