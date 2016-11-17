cebc560f43fb27eb6357d397b6fe69176fbe9ffe
Peizhen Zheng <pzzheng@thoughtworks.com>
2016-11-17 08:06:57 +0800

    roll to hospital, get into hospital for 3 days and end turn

---
+++ rich_game_mechnism/src/com/tw/core/places/Hospital.java
@@ -0,0 +1,16 @@
+package com.tw.core.places;
+
+import com.tw.core.Player;
+
+/**
+ * Created by pzzheng on 11/17/16.
+ */
+public class Hospital implements Place{
+    private static int HOSPITAL_DAYS = 2;
+    @Override
+    public Player.Status comeHere(Player player) {
+        player.moveTo(this);
+        player.stuckFor(HOSPITAL_DAYS);
+        return player.endTurn();
+    }
+}
